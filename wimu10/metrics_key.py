9ecb2d28f780b3f3eeea4c197803394e6dfd0095
Jakub Sadowski <jakub.sadowski4.stud@pw.edu.pl>
2024-03-29 15:54:24 +0100

    atempt to create function returning key for periods of music

--- wimu10/metrics_key.py
+++ wimu10/metrics_key.py
@@ -3,32 +3,14 @@ import music21 as mus
 from collections.abc import Collection
 
 #file_path = "WIMU-MIDI-METRICS\data\generated\gmt\gmt-000.mid"
-file_path = "amaj.mid"
-
-def find_key_signature_aradnessen(midi_file_path):
-    # Wczytaj plik MIDI
-    midi_stream = mus.converter.parse(midi_file_path)
-    
-    #ana = analysis.discrete.analyzeStream(midi_stream, 'aarden')
-    # key.aarden
-    # key
-    # key.krumhansl
-    # key.bellman
-    # key.simple
-    # key.temperley
-    ana =  midi_stream.analyze('key.krumhansl')
-    print(ana.correlationCoefficient)
-    print(ana.tonalCertainty())
-    print(type(midi_stream))
-    # for i in range(0,180,20):
-    #     s = midi_stream.measures(i, i+20)
-    #     a = s.analyze('aarden')
-    #     print(a)
-    #     #print(a.correlationCoefficient)
-    #     #print(a.tonalCertainty())
-    return ana
-    
+file_path = "symphon.mid"    
 midi_stream = mus.converter.parse(file_path)
+def calc_measure(stri):
+    num_measures = 0
+    for part in stri.parts:
+        # Zlicz takti w każdej części
+        num_measures += len(part.getElementsByClass(mus.stream.Measure))
+    return num_measures
     # key.aarden
     # key
     # key.krumhansl
@@ -36,11 +18,28 @@ midi_stream = mus.converter.parse(file_path)
     # key.simple
     # key.temperley
 
-def get_key_from_stream(steram: mus.stream.base.Score, alg_name: str = "key.aarden")-> Collection[str, (int, int)]:
-    analize = steram.analyze(alg_name)
+def get_key_from_stream(stream: mus.stream.base.Score, alg_name: str = "key.aarden")-> Collection[str, (int, int)]:
+    analize = stream.analyze(alg_name)
     return str(analize), (analize.correlationCoefficient, analize.tonalCertainty()) 
 
-a, b = get_key_from_stream(midi_stream)
+def get_keys_from_divided_stream(stream: mus.stream.base.Score, alg_name: str = "key.aarden", amount: int=1 )-> Collection[int]:
+    key_list = []
+    if amount <=0:
+        pass
+    else:
+        stream_len = calc_measure(stream)
+        print(len(stream))
+        step = max(int(stream_len/amount), 1)
+        for i in range(0, stream_len-step,  step):
+            end_stream = min(i+step, stream_len)
+            new_stream = stream.measures(i, end_stream)
+            key, _ = get_key_from_stream(stream=new_stream, alg_name=alg_name)
+            key_list.append(key)
+    return key_list
+
+
+a, b = get_key_from_stream(midi_stream.measures(0, 20, collect='TimeSignature'))
 print(a)
 print(type(a))
 print(b)
+print(get_keys_from_divided_stream(midi_stream, amount=8))
