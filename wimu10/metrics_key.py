8df9bd141cd943de46a961aae07b48dabf357209
Jakub Sadowski <jakub.sadowski4.stud@pw.edu.pl>
2024-03-29 23:55:59 +0100

    adding compute_ky_signatures_hist and little cleanup

--- wimu10/metrics_key.py
+++ wimu10/metrics_key.py
@@ -1,45 +1,56 @@
 
 import music21 as mus
 from collections.abc import Collection
+import muspy as mp
+from typing import Dict, List
+from muspy.utils import NOTE_MAP
 
-#file_path = "WIMU-MIDI-METRICS\data\generated\gmt\gmt-000.mid"
-file_path = "symphon.mid"    
+file_path = "amaj.mid"    
 midi_stream = mus.converter.parse(file_path)
-def calc_measure(stri):
-    num_measures = 0
-    for part in stri.parts:
-        # Zlicz takti w każdej części
-        num_measures += len(part.getElementsByClass(mus.stream.Measure))
-    return num_measures
+
+
+INVERT_NOTE_MAP: Dict[int, str] = {v: k for k, v in NOTE_MAP.items()}
+
+class KeyDuration:
+    def __init__(self, start:int, stop:int, key:str) -> None:
+        self.start = start
+        self.stop = stop
+        self.key = key
+        
+
+def get_key_from_music21_stream(stream: mus.stream.base.Score, alg_name: str = "key.aarden")-> Collection[str, (int, int)]:
     # key.aarden
     # key
     # key.krumhansl
     # key.bellman
     # key.simple
     # key.temperley
-
-def get_key_from_stream(stream: mus.stream.base.Score, alg_name: str = "key.aarden")-> Collection[str, (int, int)]:
     analize = stream.analyze(alg_name)
     return str(analize), (analize.correlationCoefficient, analize.tonalCertainty()) 
 
-def get_keys_from_divided_stream(stream: mus.stream.base.Score, alg_name: str = "key.aarden", amount: int=1 )-> Collection[int]:
-    key_list = []
-    if amount <=0:
-        pass
-    else:
-        stream_len = calc_measure(stream)
-        print(len(stream))
-        step = max(int(stream_len/amount), 1)
-        for i in range(0, stream_len-step,  step):
-            end_stream = min(i+step, stream_len)
-            new_stream = stream.measures(i, end_stream)
-            key, _ = get_key_from_stream(stream=new_stream, alg_name=alg_name)
-            key_list.append(key)
-    return key_list
+def get_key_from_muspy_music(music: mp.Music, alg_name: str = "key.aarden")-> Collection[str, (int, int)]:
+    stream = mp.to_music21(music)
+    return get_key_from_music21_stream(stream=stream, alg_name=alg_name)
+
+def compute_key_signatures_hist(music: mp.Music)->Collection[List[int], List[int]]:
+    key_signature = music.key_signatures
+    hist_maj = [0] * 12
+    hist_min = [0] *12
+    for key in key_signature:
+        if key.mode == "major":
+            hist_maj[key.root] += 1
+        elif  key.mode == "minor":
+            hist_min[key.root] +=2
+    return hist_maj, hist_min
 
 
-a, b = get_key_from_stream(midi_stream.measures(0, 20, collect='TimeSignature'))
+
+a, b = get_key_from_music21_stream(midi_stream.measures(0, 20, collect='TimeSignature'))
 print(a)
-print(type(a))
-print(b)
-print(get_keys_from_divided_stream(midi_stream, amount=8))
+
+track = mp.read_midi(file_path)
+strin11 = mp.to_music21(track)
+print("aaala ", strin11.analyze("key"))
+print(get_key_from_muspy_music(track))
+
+print(track.key_signatures)
