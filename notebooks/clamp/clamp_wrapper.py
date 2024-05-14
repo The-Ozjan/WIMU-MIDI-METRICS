bf5099d2d9232fc08af352c9a7f5cfd3e7a985ab
Jakub Sadowski <you@example.com>
2024-05-14 22:56:24 +0200

    creating plots with clamp tests

--- notebooks/clamp/clamp_wrapper.py
+++ notebooks/clamp/clamp_wrapper.py
@@ -1,6 +1,8 @@
 import subprocess
+from typing import Dict, List, Tuple
+import shutil
 
-def clamp_output(query_modal: str, key_modal:str, top_n:int = 1, model:str = "sander-wood/clamp-small-512", cwd: str = "notebooks/clamp/model/"):
+def clamp_output(query_modal: str, key_modal:str, top_n:int = 1, model:str = "sander-wood/clamp-small-512", cwd: str = "notebooks/clamp/model/") -> List[Tuple[str, float, float]]:
     if top_n <= 0:
         return []
     command = ["python", "clamp_ori.py", "-clamp_model_name", model,
@@ -14,7 +16,7 @@ def clamp_output(query_modal: str, key_modal:str, top_n:int = 1, model:str = "sa
         if line.startswith("Prob"):
             prob = float(line.split(':')[1].split('%')[0]) / 100  # Extract probability
             sim = float(line.split('- Sim:')[1].split(':')[0])  # Extract similarity
-            music_name = data_lines[i+1].strip().split('inference/music_keys/')[1]  # Extract music file name
+            music_name = data_lines[i+1].strip().split('/')[-1]  # Extract music file name
             music_data.append((music_name, prob, sim))
             i += 2  # Skip the next line since it's the music file name
         else:
@@ -22,6 +24,22 @@ def clamp_output(query_modal: str, key_modal:str, top_n:int = 1, model:str = "sa
 
     return music_data
 
-def change_clamp_txt_query(query:str, path:str="notebooks/clamp/model/inference/"):
+def change_clamp_txt_query(query:str, path:str="notebooks/clamp/model/inference/") -> None:
     with open(path + "text_query.txt", 'w') as file:
         file.write(query)
+
+def get_output_change_txt_query(query:str, path:str="notebooks/clamp/model/inference/", top_n:int = 1, model:str = "sander-wood/clamp-small-512", cwd: str = "notebooks/clamp/model/") -> List[Tuple[str, float, float]]:
+    change_clamp_txt_query(query, path)
+    return clamp_output("text", "music",top_n=top_n, model=model, cwd=cwd)
+
+def change_clamp_music_query(original_query_path:str, clamp_reading_path:str="notebooks/clamp/model/inference/") -> None:
+    shutil.copy2(original_query_path, clamp_reading_path + "music_query.mxl")
+
+def change_clamp_txt_keys(keys:List[str], path:str="notebooks/clamp/model/inference/") -> None:
+    with open(path + "text_keys.txt", 'w') as file:
+        for key in keys:
+            file.write(key+"\n")
+
+def get_output_change_music_query(music_query_path:str, path:str="notebooks/clamp/model/inference/", top_n:int = 1, model:str = "sander-wood/clamp-small-512", cwd: str = "notebooks/clamp/model/")-> List[Tuple[str, float, float]]:
+    change_clamp_music_query(music_query_path, path)
+    return clamp_output("music", "text", top_n=top_n, model=model, cwd=cwd)
--- notebooks/clamp_test.py
+++ notebooks/clamp_test.py
@@ -1,4 +1,48 @@
+import matplotlib.pyplot as plt
 from notebooks.clamp import clamp_wrapper as cw
+import numpy as np 
+import matplotlib.pyplot as plt 
+from typing import Dict, List, Tuple
 
-cw.change_clamp_txt_query("Film music.")
-print(cw.clamp_output("text", "music", top_n=5))
+def test(query:str):
+    cw.change_clamp_txt_query(query)
+    return cw.clamp_output("text", "music", top_n=24)
+
+def save_plot(smaller:List[Tuple[str, int, int]], bigger:List[Tuple[str, int, int]], query:str) ->None:
+    barWidth = 0.25
+    fig = plt.subplots(figsize =(12, 8)) 
+    br1 = np.arange(len(smaller)) 
+    br2 = [x + barWidth for x in br1] 
+    smaller.sort()
+    bigger.sort()
+    plt.bar(br1, [tup[1] for tup in smaller], color ='r', width = barWidth, 
+            edgecolor ='grey', label ='clamp-small-512') 
+    plt.bar(br2, [tup[1] for tup in bigger], color ='g', width = barWidth, 
+            edgecolor ='grey', label ='clamp-small-1024') 
+    plt.xlabel('Music name', fontweight ='bold', fontsize = 15) 
+    plt.ylabel('Probability', fontweight ='bold', fontsize = 15) 
+    plt.xticks([r + barWidth for r in range(len(smaller))], 
+            [tup[0] for tup in smaller], rotation=90)
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(query+'.png')
+
+def save_plot_clamp_models_txt_query(query:str, top_n:int=24) -> None:
+    smaller = cw.get_output_change_txt_query(query, top_n=top_n)
+    bigger = cw.clamp_output("text", "music", top_n=24, model="sander-wood/clamp-small-1024")
+    save_plot(smaller, bigger, query)
+
+def save_plot_clamp_models_music_query(query:str, top_n:int = 24) -> None:
+    smaller = cw.get_output_change_music_query(query, top_n=top_n)
+    bigger = cw.clamp_output("music", "text", top_n=24, model="sander-wood/clamp-small-1024")
+    save_plot(smaller, bigger, query.split('/')[-1])
+
+
+def main():
+    cw.change_clamp_txt_keys(["It is funny.", "It is sad.", "It is about love", "It is composed for movie", "Clint Eastwood played in fil with this music."])
+    save_plot_clamp_models_txt_query("Love song.")
+    save_plot_clamp_models_music_query("notebooks/clamp/model/inference/music_keys/Gran Torino.mxl")
+
+
+if __name__ == "__main__":
+    main()
