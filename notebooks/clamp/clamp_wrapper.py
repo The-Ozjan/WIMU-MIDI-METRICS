a5d8dd7916d11bb1f141eafa773f23996685dcbb
Jsadows <j.sadowski09@gmail.com>
2024-05-19 21:22:22 +0200

    clamp code cleanup

--- notebooks/clamp/clamp_wrapper.py
+++ notebooks/clamp/clamp_wrapper.py
@@ -2,7 +2,14 @@ import subprocess
 from typing import Dict, List, Tuple
 import shutil
 
-def clamp_output(query_modal: str, key_modal:str, top_n:int = 1, model:str = "sander-wood/clamp-small-512", cwd: str = "notebooks/clamp/model/") -> List[Tuple[str, float, float]]:
+
+def clamp_output(
+    query_modal: str,
+    key_modal: str,
+    top_n: int = 1,
+    model: str = 'sander-wood/clamp-small-512',
+    cwd: str = 'notebooks/clamp/model/',
+) -> List[Tuple[str, float, float]]:
     """
     Function for getting output from clamp model using different queries
     Returns list of tupples with music name, probability and similarity
@@ -14,18 +21,28 @@ def clamp_output(query_modal: str, key_modal:str, top_n:int = 1, model:str = "sa
     """
     if top_n <= 0:
         return []
-    command = ["python", "clamp_ori.py", "-clamp_model_name", model,
-            "-query_modal", query_modal, "-key_modal", key_modal, "-top_n", str(top_n)]
-    res = subprocess.run(command,stdout=subprocess.PIPE, cwd = cwd)
+    command = [
+        'python',
+        'clamp_ori.py',
+        '-clamp_model_name',
+        model,
+        '-query_modal',
+        query_modal,
+        '-key_modal',
+        key_modal,
+        '-top_n',
+        str(top_n),
+    ]
+    res = subprocess.run(command, stdout=subprocess.PIPE, cwd=cwd)
     data_lines = res.stdout.decode('utf-8').strip().split('\n')
     music_data = []
     i = 0
     while i < len(data_lines):
         line = data_lines[i]
-        if line.startswith("Prob"):
+        if line.startswith('Prob'):
             prob = float(line.split(':')[1].split('%')[0]) / 100  # Extract probability
             sim = float(line.split('- Sim:')[1].split(':')[0])  # Extract similarity
-            music_name = data_lines[i+1].strip().split('/')[-1]  # Extract music file name
+            music_name = data_lines[i + 1].strip().split('/')[-1]  # Extract music file name
             music_data.append((music_name, prob, sim))
             i += 2  # Skip the next line since it's the music file name
         else:
@@ -33,17 +50,25 @@ def clamp_output(query_modal: str, key_modal:str, top_n:int = 1, model:str = "sa
 
     return music_data
 
-def change_clamp_txt_query(query:str, path:str="notebooks/clamp/model/inference/") -> None:
+
+def change_clamp_txt_query(query: str, path: str = 'notebooks/clamp/model/inference/') -> None:
     """
     Function for clamp text query changing
 
     query: new string query
     path: path to query file to save
     """
-    with open(path + "text_query.txt", 'w') as file:
+    with open(path + 'text_query.txt', 'w') as file:
         file.write(query)
 
-def get_output_change_txt_query(query:str, path:str="notebooks/clamp/model/inference/", top_n:int = 1, model:str = "sander-wood/clamp-small-512", cwd: str = "notebooks/clamp/model/") -> List[Tuple[str, float, float]]:
+
+def get_output_change_txt_query(
+    query: str,
+    path: str = 'notebooks/clamp/model/inference/',
+    top_n: int = 1,
+    model: str = 'sander-wood/clamp-small-512',
+    cwd: str = 'notebooks/clamp/model/',
+) -> List[Tuple[str, float, float]]:
     """
     Function for getting text query clamp output.
     Returns clamp model output for text guery
@@ -55,29 +80,38 @@ def get_output_change_txt_query(query:str, path:str="notebooks/clamp/model/infer
     cwd: working directory for prosess
     """
     change_clamp_txt_query(query, path)
-    return clamp_output("text", "music",top_n=top_n, model=model, cwd=cwd)
+    return clamp_output('text', 'music', top_n=top_n, model=model, cwd=cwd)
 
-def change_clamp_music_query(original_query_path:str, clamp_reading_path:str="notebooks/clamp/model/inference/") -> None:
+
+def change_clamp_music_query(original_query_path: str, clamp_reading_path: str = 'notebooks/clamp/model/inference/') -> None:
     """
     Function for changing music query.
 
     original_query_path: original music query
     clamp_reading_path: path to clmp
     """
-    shutil.copy2(original_query_path, clamp_reading_path + "music_query.mxl")
+    shutil.copy2(original_query_path, clamp_reading_path + 'music_query.mxl')
 
-def change_clamp_txt_keys(keys:List[str], path:str="notebooks/clamp/model/inference/") -> None:
+
+def change_clamp_txt_keys(keys: List[str], path: str = 'notebooks/clamp/model/inference/') -> None:
     """
     Function for clamp text keys changing.
 
     keys: list with string keys
     path: path to clamp
     """
-    with open(path + "text_keys.txt", 'w') as file:
+    with open(path + 'text_keys.txt', 'w') as file:
         for key in keys:
-            file.write(key+"\n")
+            file.write(key + '\n')
 
-def get_output_change_music_query(music_query_path:str, path:str="notebooks/clamp/model/inference/", top_n:int = 1, model:str = "sander-wood/clamp-small-512", cwd: str = "notebooks/clamp/model/")-> List[Tuple[str, float, float]]:
+
+def get_output_change_music_query(
+    music_query_path: str,
+    path: str = 'notebooks/clamp/model/inference/',
+    top_n: int = 1,
+    model: str = 'sander-wood/clamp-small-512',
+    cwd: str = 'notebooks/clamp/model/',
+) -> List[Tuple[str, float, float]]:
     """
     Function for getting output for clamp model and music query changing
 
@@ -88,9 +122,16 @@ def get_output_change_music_query(music_query_path:str, path:str="notebooks/clam
     cwd: working directory for prosess
     """
     change_clamp_music_query(music_query_path, path)
-    return clamp_output("music", "text", top_n=top_n, model=model, cwd=cwd)
+    return clamp_output('music', 'text', top_n=top_n, model=model, cwd=cwd)
 
-def get_output_music_to_music_change_music_query(music_query_path:str, path:str="notebooks/clamp/model/inference/", top_n:int = 1, model:str = "sander-wood/clamp-small-512", cwd: str = "notebooks/clamp/model/")-> List[Tuple[str, float, float]]:
+
+def get_output_music_to_music_change_music_query(
+    music_query_path: str,
+    path: str = 'notebooks/clamp/model/inference/',
+    top_n: int = 1,
+    model: str = 'sander-wood/clamp-small-512',
+    cwd: str = 'notebooks/clamp/model/',
+) -> List[Tuple[str, float, float]]:
     """
     Function for getting output for clamp model and music query changing for music to music comparsion.
 
@@ -101,4 +142,4 @@ def get_output_music_to_music_change_music_query(music_query_path:str, path:str=
     cwd: working directory for prosess
     """
     change_clamp_music_query(music_query_path, path)
-    return clamp_output("music", "music", top_n=top_n, model=model, cwd=cwd)
+    return clamp_output('music', 'music', top_n=top_n, model=model, cwd=cwd)
--- notebooks/clamp_test.py
+++ notebooks/clamp_test.py
@@ -1,68 +1,82 @@
 import matplotlib.pyplot as plt
 from notebooks.clamp import clamp_wrapper as cw
-import numpy as np 
-import matplotlib.pyplot as plt 
+import numpy as np
+import matplotlib.pyplot as plt
 from typing import Dict, List, Tuple
 
-def save_plot(smaller:List[Tuple[str, int, int]], bigger:List[Tuple[str, int, int]], query:str) ->None:
+
+def save_plot(smaller: List[Tuple[str, int, int]], bigger: List[Tuple[str, int, int]], query: str) -> None:
     barWidth = 0.25
-    fig = plt.subplots(figsize =(12, 8)) 
-    br1 = np.arange(len(smaller)) 
-    br2 = [x + barWidth for x in br1] 
+    fig = plt.subplots(figsize=(12, 8))
+    br1 = np.arange(len(smaller))
+    br2 = [x + barWidth for x in br1]
     smaller.sort()
     bigger.sort()
-    plt.bar(br1, [tup[1] for tup in smaller], color ='r', width = barWidth, 
-            edgecolor ='grey', label ='clamp-small-512') 
-    plt.bar(br2, [tup[1] for tup in bigger], color ='g', width = barWidth, 
-            edgecolor ='grey', label ='clamp-small-1024') 
-    plt.xlabel('Music name', fontweight ='bold', fontsize = 15) 
-    plt.ylabel('Probability', fontweight ='bold', fontsize = 15) 
-    plt.xticks([r + barWidth for r in range(len(smaller))], 
-            [tup[0] for tup in smaller], rotation=90)
+    plt.bar(br1, [tup[1] for tup in smaller], color='r', width=barWidth, edgecolor='grey', label='clamp-small-512')
+    plt.bar(br2, [tup[1] for tup in bigger], color='g', width=barWidth, edgecolor='grey', label='clamp-small-1024')
+    plt.xlabel('Music name', fontweight='bold', fontsize=15)
+    plt.ylabel('Probability', fontweight='bold', fontsize=15)
+    plt.xticks([r + barWidth for r in range(len(smaller))], [tup[0] for tup in smaller], rotation=90)
     plt.legend()
     plt.tight_layout()
-    plt.savefig(query+'.png')
+    plt.savefig(query + '.png')
 
-def save_plot_clamp_models_txt_query(query:str, top_n:int=24) -> None:
+
+def save_plot_clamp_models_txt_query(query: str, top_n: int = 24) -> None:
     smaller = cw.get_output_change_txt_query(query, top_n=top_n)
-    bigger = cw.clamp_output("text", "music", top_n=24, model="sander-wood/clamp-small-1024")
+    bigger = cw.clamp_output('text', 'music', top_n=24, model='sander-wood/clamp-small-1024')
     save_plot(smaller, bigger, query)
 
-def save_plot_clamp_models_music_query(query:str, top_n:int = 24) -> None:
+
+def save_plot_clamp_models_music_query(query: str, top_n: int = 24) -> None:
     smaller = cw.get_output_change_music_query(query, top_n=top_n)
-    bigger = cw.clamp_output("music", "text", top_n=24, model="sander-wood/clamp-small-1024")
+    bigger = cw.clamp_output('music', 'text', top_n=24, model='sander-wood/clamp-small-1024')
     save_plot(smaller, bigger, query.split('/')[-1])
 
-def save_plot_clamp_models_music_query_music_keys(query:str, top_n:int = 24) -> None:
+
+def save_plot_clamp_models_music_query_music_keys(query: str, top_n: int = 24) -> None:
     smaller = cw.get_output_music_to_music_change_music_query(query, top_n=top_n)
-    bigger = cw.clamp_output("music", "music", top_n=24, model="sander-wood/clamp-small-1024")
-    save_plot(smaller, bigger, "m2m" + query.split('/')[-1])
+    bigger = cw.clamp_output('music', 'music', top_n=24, model='sander-wood/clamp-small-1024')
+    save_plot(smaller, bigger, 'm2m' + query.split('/')[-1])
 
 
 def main():
-    # save_plot_clamp_models_txt_query("Love song.")
-    # save_plot_clamp_models_txt_query("This song genre is Jazz")
-    # save_plot_clamp_models_txt_query("This song genre is Dance")
-    # save_plot_clamp_models_txt_query("This song genre is Rock")
-    # save_plot_clamp_models_txt_query("This song genre is Country")
-    # save_plot_clamp_models_txt_query("This song is Happy")
-    # save_plot_clamp_models_txt_query("This song is from movie")
+    save_plot_clamp_models_txt_query("Love song.")
+    save_plot_clamp_models_txt_query("This song genre is Jazz")
+    save_plot_clamp_models_txt_query("This song genre is Dance")
+    save_plot_clamp_models_txt_query("This song genre is Rock")
+    save_plot_clamp_models_txt_query("This song genre is Country")
+    save_plot_clamp_models_txt_query("This song is Happy")
+    save_plot_clamp_models_txt_query("This song is from movie")
 
-    cw.change_clamp_txt_keys(["It is funny.", "It is sad.", "It is about love.", "It is composed for movie.", "Clint Eastwood played in film with this music.", "Genre of this music is Jazz", "Genre of this music is Reggae.", "Genre of this music is Rock.", "Genre of this music is Classic.", "This music could be in mafia movie."])
-    # save_plot_clamp_models_music_query("notebooks/clamp/model/inference/music_keys/Gran Torino.mxl")
-    # save_plot_clamp_models_music_query("notebooks/clamp/model/inference/music_keys/Don't Worry, Be Happy.mxl")
-    # save_plot_clamp_models_music_query("notebooks/clamp/model/inference/music_keys/Flight of the Bumblebee.mxl")
-    # save_plot_clamp_models_music_query("notebooks/clamp/model/inference/music_keys/Call Me Maybe.mxl")
-    # save_plot_clamp_models_music_query("notebooks/clamp/model/inference/music_keys/The Godfather Theme.mxl")
-    # save_plot_clamp_models_music_query("notebooks/clamp/model/inference/music_keys/Eye of the Tiger.mxl")
+    cw.change_clamp_txt_keys(
+        [
+            'It is funny.',
+            'It is sad.',
+            'It is about love.',
+            'It is composed for movie.',
+            'Clint Eastwood played in film with this music.',
+            'Genre of this music is Jazz',
+            'Genre of this music is Reggae.',
+            'Genre of this music is Rock.',
+            'Genre of this music is Classic.',
+            'This music could be in mafia movie.',
+        ]
+    )
+    save_plot_clamp_models_music_query("notebooks/clamp/model/inference/music_keys/Gran Torino.mxl")
+    save_plot_clamp_models_music_query("notebooks/clamp/model/inference/music_keys/Don't Worry, Be Happy.mxl")
+    save_plot_clamp_models_music_query("notebooks/clamp/model/inference/music_keys/Flight of the Bumblebee.mxl")
+    save_plot_clamp_models_music_query("notebooks/clamp/model/inference/music_keys/Call Me Maybe.mxl")
+    save_plot_clamp_models_music_query("notebooks/clamp/model/inference/music_keys/The Godfather Theme.mxl")
+    save_plot_clamp_models_music_query("notebooks/clamp/model/inference/music_keys/Eye of the Tiger.mxl")
 
-    save_plot_clamp_models_music_query_music_keys("notebooks/clamp/model/inference/music_keys/Gran Torino.mxl")
+    save_plot_clamp_models_music_query_music_keys('notebooks/clamp/model/inference/music_keys/Gran Torino.mxl')
     save_plot_clamp_models_music_query_music_keys("notebooks/clamp/model/inference/music_keys/Don't Worry, Be Happy.mxl")
-    save_plot_clamp_models_music_query_music_keys("notebooks/clamp/model/inference/music_keys/Flight of the Bumblebee.mxl")
-    save_plot_clamp_models_music_query_music_keys("notebooks/clamp/model/inference/music_keys/Call Me Maybe.mxl")
-    save_plot_clamp_models_music_query_music_keys("notebooks/clamp/model/inference/music_keys/The Godfather Theme.mxl")
-    save_plot_clamp_models_music_query_music_keys("notebooks/clamp/model/inference/music_keys/Eye of the Tiger.mxl")
+    save_plot_clamp_models_music_query_music_keys('notebooks/clamp/model/inference/music_keys/Flight of the Bumblebee.mxl')
+    save_plot_clamp_models_music_query_music_keys('notebooks/clamp/model/inference/music_keys/Call Me Maybe.mxl')
+    save_plot_clamp_models_music_query_music_keys('notebooks/clamp/model/inference/music_keys/The Godfather Theme.mxl')
+    save_plot_clamp_models_music_query_music_keys('notebooks/clamp/model/inference/music_keys/Eye of the Tiger.mxl')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
