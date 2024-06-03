0f2eb0db6d6b99191ef1172b8404551e01fede58
wtargosi <01142543@pw.edu.pl>
2024-06-03 23:04:12 +0200

    add clamp experiment data

--- notebooks/dyn_test.py
+++ notebooks/dyn_test.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-# from .clamp.model.clamp import main as create_embeddings
 import numpy as np
 from .clamp.statistics import load_json_as_ndarray, normalize_embeddings
 import matplotlib.pyplot as plt
@@ -7,50 +6,44 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
-    # create_embeddings(Path('aaa'), Path('aaa/emb'), 64)
-
-    # dodawać do wektora kolejno ciche, głośne i dziwne
-    embeddings_dir = Path('aaa/emb')
+    embeddings_dir = Path('data/clamp_ebd_test/emb')
     embedding_files = list(embeddings_dir.glob('**/*.json'))
     names = list(f'{f.stem}.midi' for f in embedding_files)
     embeddings = np.stack(list(load_json_as_ndarray(f) for f in embedding_files))
     embeddings = normalize_embeddings(embeddings)
     dist_matrix = np.zeros((embeddings.shape[0], embeddings.shape[0]))
 
-    # for i in range(embeddings.shape[0]):
-    #     for j in range(embeddings.shape[0]):
-    #         dist_matrix[i, j] = np.linalg.norm(embeddings[i] - embeddings[j])
+    plt.imshow(embeddings)
+    plt.show()
 
-    # plt.imshow(dist_matrix)
-    # plt.show()
-    # wyświetlić wektor 2D w matplotlib
-    # Inicjalizacja wynikowych macierzy
+    for i in range(embeddings.shape[0]):
+        for j in range(embeddings.shape[0]):
+            dist_matrix[i, j] = np.linalg.norm(embeddings[i] - embeddings[j])
+
+    plt.imshow(dist_matrix)
+    plt.show()
+
     min_wyniki = np.zeros((16, 16, 2), dtype=float)
     max_wyniki = np.zeros((16, 16, 2), dtype=float)
 
     # Iteracja przez wszystkie pary punktów
     for i in range(16):
         for j in range(16):
-            if i != j:  # Unikanie porównywania tego samego punktu z samym sobą
-                # Obliczenie różnic w każdej współrzędnej
+            if i != j:
                 roznice = np.abs(embeddings[i] - embeddings[j])
-                # Znalezienie współrzędnej, w której różnią się najmniej i najbardziej
                 min_indeks = np.argmin(roznice)
                 max_indeks = np.argmax(roznice)
-                # Zapisanie wyników
                 min_wyniki[i, j] = [min_indeks, roznice[min_indeks]]
                 max_wyniki[i, j] = [max_indeks, roznice[max_indeks]]
 
-    print(min_wyniki)
-    print(max_wyniki)
 
     plt.subplot(1, 2, 1)
-    plt.imshow(min_wyniki[:,:,0], cmap='viridis', interpolation='nearest')
+    plt.imshow(min_wyniki[:,:,1], cmap='viridis', interpolation='nearest')
     plt.colorbar()
     plt.title('Indeks najmniejszej różnicy')
 
     plt.subplot(1, 2, 2)
-    plt.imshow(max_wyniki[:,:,0], cmap='viridis', interpolation='nearest')
+    plt.imshow(max_wyniki[:,:,1], cmap='viridis', interpolation='nearest')
     plt.colorbar()
     plt.title('Indeks największej różnicy')
 
